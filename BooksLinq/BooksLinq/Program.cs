0d232350ad7f3589029aea640ff645c7558f7a1f
mhmyhan <max.myhan@gmail.com>
2024-10-03 12:10:55 +0100

    Cleanup

--- BooksLinq/BooksLinq/Program.cs
+++ BooksLinq/BooksLinq/Program.cs
@@ -4,12 +4,6 @@ using BooksLinq;
 
 Console.WriteLine("Book List LINQ\n");
 
-var books = new List<Book>
-{
-    new Book(1, "the Catcher in the Rye", "J.D. Salinger", 227,
-        "Little, Brown and Company", 1951, "Fiction", true),
-};
-
 static void DisplayBookList(List<Book> bookList)
 {
     foreach (var book in bookList)
@@ -19,10 +13,41 @@ static void DisplayBookList(List<Book> bookList)
     
 }
 
-DisplayBookList(books);
+// create book list and add options
+var books = new List<Book>();
 
 books.AddRange(new List<Book>
 {
+    new Book(1, "the Catcher in the Rye", "J.D. Salinger", 227,
+        "Little, Brown and Company", 1951, "Fiction", true),
+    new Book(4, "The Hobbit", "J.R.R. Tolkien", 310,
+        "George Allen & Unwin", 1937, "Fantasy", true),
+    new Book(5, "Tender Is the Night", "F. Scott Fitzgerald", 317,
+        "Charles Scribner", 1934, "Fiction", true),
+    new Book(6, "The Great Gatsby", "F. Scott Fitzgerald", 180,
+        "Charles Scribner", 1925, "Fiction", true),
+    new Book(7, "To Kill a Mockingbird", "Harper Lee", 336,
+        "J.B. Lippincott & Co.", 1960, "Fiction", true),
+    new Book(8, "The Alchemist", "Paulo Coelho", 208,
+        "HarperTorch", 1988, "Adventure", true),
+    new Book(9, "Brave New World", "Aldous Huxley", 268,
+        "Chatto & Windus", 1932, "Dystopian", true),
+    new Book(10, "Moby-Dick", "Herman Melville", 635,
+        "Harper & Brothers", 1851, "Adventure", true),
+    new Book(11, "Pride and Prejudice", "Jane Austen", 279,
+        "T. Egerton, Whitehall", 1813, "Romance", true),
+    new Book(12, "The Lord of the Rings", "J.R.R. Tolkien", 1178,
+        "George Allen & Unwin", 1954, "Fantasy", true),
+    new Book(13, "Becoming", "Michelle Obama", 448,
+        "Crown", 2018, "Autobiography", false),
+    new Book(14, "Sapiens: A Brief History of Humankind", "Yuval Harari", 443,
+        "Harper", 2011, "Non-Fiction", false),
+    new Book(15, "Educated", "Tara Westover", 334,
+        "Random House", 2018, "Autobiography", false),
+    new Book(17, "Thinking, Fast and Slow", "Daniel Kahneman", 499,
+        "Farrar, Straus and Giroux", 2011, "Non-Fiction", false),
+    new Book(18, "The Body Keeps the Score", "Bessel van der Kolk", 464,
+        "Viking", 2014, "Non-Fiction", false),
     new Book(19, "1984", "George Orwell", 328,
         "Secker & Warburg", 1949, "Dystopian", true),
     new Book(20, "Crime and Punishment", "Fyodor Dostoevsky", 671,
@@ -74,11 +99,4 @@ var tolkienBooks = books
 
 DisplayBookList(tolkienBooks);
 
-Console.WriteLine("\nTolkien Books");
-
-var tolkienBooks = books
-    .Where(b => b.Author == "Tolkien")
-    .ToList();
-
-DisplayBookList(tolkienBooks);
 
